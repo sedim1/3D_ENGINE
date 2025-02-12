52b0600a8e4cb56b1f74e55672463b811cb092b7
sedim1 <elsedim09@gmail.com>
2025-02-12 13:46:01 -0600

    BONE.cpp and BONE.h

---
+++ include/BONE.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<assimp/Importer.hpp>
+#include<assimp/scene.h>
+#include<assimp/Importer.hpp>
+
+struct KeyPosition{
+	glm::vec3 position;
+	float timeStamp;
+};
+
+struct KeyRotation{
+};
